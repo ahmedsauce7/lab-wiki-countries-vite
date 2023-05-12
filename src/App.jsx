8ffa3ea7e69465d90ebf2ca3c4fea75f43584d07
ahmed awad <ahmeedsauce7@gmail.com>
2023-05-12 13:02:00 +0300

    iteration 4 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
 // src/App.jsx
 import './App.css'
-import countriesJSON from './countries.json'
-import { useState } from 'react'
+// import countriesJSON from './countries.json'
+import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from './components/Navbar'
 import CountriesList from './components/CountriesList'
 import CountryDetails from './components/CountryDetails'
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios'
 
 
 function App() {
-  const [countries, setCountries] = useState(countriesJSON);
+  const [countries, setCountries] = useState([]);
+  // iteration 4 using fetch 
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://ih-countries-api.herokuapp.com/countries")
+      const parsed = await response.json()
+      console.log(parsed)
+      setCountries(parsed)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(() => {
+      fetchData() 
+    }, []
+    )
+
+  // iteration 4 using axios
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get("https://ih-countries-api.herokuapp.com/countries")
+  //     console.log(response.status)
+  //     if (response.status === 200){
+  //       setCountries(response.data)
+  //     }
+  //   } catch (error) {
+  //     console.log(error) 
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetchData() 
+  // }, []
+  // )
+
   return <div className='App'>
     <Navbar />
     <div className="mainPage">
